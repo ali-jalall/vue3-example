08d20a57976aef179132eb133a4087b66d2db29a
alijalal-sys <ajmabilal@gmail.com>
2020-05-16 02:37:34 +0200

    new emits feature

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="modal">
     <slot></slot>
+    <button @click="$emit('close')">Dismiss</button>
   </div>
 </template>
+<script>
+export default {
+  emits: [ "close" ]
+}
+</script>
