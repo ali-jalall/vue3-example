9190c5332b50eb7f2cf9b25e8d8d23b45fc50f7f
alijalal-sys <ajmabilal@gmail.com>
2020-05-16 02:39:16 +0200

    modal detctions

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <button @click="toggleModalState">Open modal</button>
   <teleport to="#modal-wrapper">
-    <modal v-if="modalState">
+    <modal v-if="modalState" @click="toggleModalState">
       <p>Hello, I'm a modal window.</p>
     </modal>
   </teleport>
@@ -10,15 +10,15 @@
 import Modal from "./components/Modal.vue";
 import { ref } from "vue";
 export default {
-  setup () {
+  setup() {
     const modalState = ref(false);
     const toggleModalState = () => {
       modalState.value = !modalState.value;
     };
     return {
       modalState,
-      toggleModalState
-    }
-  }
+      toggleModalState,
+    };
+  },
 };
 </script>
